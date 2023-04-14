02e5ec505a9f588f34d394e87438a2ff76e63bcc
unknown <tyler.levie@gmail.com>
2023-04-13 20:08:25 -0500

    adding flask changes - aws integration template

--- app.py
+++ app.py
@@ -1,21 +1,64 @@
-# Import dependencies
-from flask import Flask, render_template, request, jsonify, g
-import sqlite3
+from flask import Flask,render_template
+import boto3, botocore
+import configparser
+import os
+from werkzeug.utils import secure_filename
+from flask import Flask, redirect, url_for, request
 import json
+import numpy as np
 
-# Create Flask app
 app = Flask(__name__)
 
-# Home endpoint on Flask app.route /
 @app.route('/')
 def home():
     return render_template('home.html')
 
-# Home endpoint on Flask app.route /js
-@app.route('/application')
-def js():
-    return render_template('application.html')
+@app.route('/application', methods=["POST","GET"])
+def application():
+   cls = ''
+   prob = 0
 
-# Run Flask app
-if __name__ == "__main__":
-    app.run(debug=True)
+   if request.method == 'POST':
+      print('called')
+      config = configparser.ConfigParser()
+      config.read('config.ini')
+
+      key = config['aws']['AWS_ACCESS_KEY']
+      secret = config['aws']['AWS_SECRET_ACCESS_KEY']
+
+      runtime = boto3.client(
+         "sagemaker-runtime",
+         aws_access_key_id=key,
+         aws_secret_access_key=secret
+      )
+
+      endpoint = 'enter endpoint here'
+
+      # Read image into memory
+      payload = request.files['file']
+
+      # Send image via InvokeEndpoint API
+      response = runtime.invoke_endpoint(EndpointName=endpoint,
+                                         ContentType='application/x-image',
+                                         Body=payload)
+
+      result = response["Body"].read()
+      result = json.loads(result)
+      index = np.argmax(result)
+
+      object_categories = [
+        #enter object categories here
+      ]
+
+      cls = object_categories[index]
+      prob = str(round(result[index],2))
+
+   return render_template('application.html',cls=cls,prob=prob)
+
+def lyric_input(lyric):
+    #todo
+    return lyric
+
+#run flask app
+if __name__ == '__main__':
+   app.run()
